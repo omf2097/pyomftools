8500effa4ade8ecb3eff0501a749a869467a8768
Tuomas Virtanen <katajakasa@gmail.com>
2016-11-22 01:57:44 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='OMFTools',
+    version='0.1',
+    description='Tools for modifying OMF2097 files',
+    author='Tuomas Virtanen',
+    author_email='katajakasa@gmail.com',
+    url='https://github.com/omf2097/pyomftools',
+    packages=['omftools', 'omftools.pyshadowdive'],
+)
